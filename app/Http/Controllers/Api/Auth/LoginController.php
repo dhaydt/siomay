23052b88d13a771b2fb86b88f3a084ac75159d83
dhaydt <1992dayat@gmail.com>
2022-07-12 13:24:43 +0700

    fix api login

---
+++ app/Http/Controllers/Api/Auth/LoginController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Http\Controllers\Api\Auth;
+
+use App\CPU\helpers;
+use App\Http\Controllers\Controller;
+use App\Models\LoginLog;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Crypt;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+
+class LoginController extends Controller
+{
+    public function login(Request $request)
+    {
+        $validate = Validator::make($request->all(), [
+            'phone' => 'required',
+            'password' => 'required|string|min:8',
+        ], [
+            'phone.required' => 'Nomor HP tidak boleh kosong',
+            'password.required' => 'Password masih kosong',
+            'password.string' => 'Password tidak valid',
+            'password.min' => 'Password kurang panjang',
+        ]);
+
+        if ($validate->fails()) {
+            return helpers::response('error', $validate->errors()->all()[0]);
+        }
+
+        $admin = User::where('phone', $request->phone)->first();
+        if (!$admin) {
+            return helpers::response('error', 'Identitas tidak ditemukan');
+        }
+
+        if (!Hash::check($request->password, $admin->password)) {
+            return helpers::response('error', 'Password salah');
+        }
+
+        $check = LoginLog::where('user_id', $admin->id)->first();
+        $token = Crypt::encryptString(now());
+
+        if (isset($check)) {
+            $check->user_id = $admin->id;
+            $check->token = $token;
+            $check->role_id = $admin->role;
+            $check->last_login = now();
+            $check->status = 1;
+            $check->save();
+
+            return helpers::response(1, 'Login Berhasil', [
+                'loginLogs' => $check,
+            ]);
+        } else {
+            $loginLogs = LoginLog::create([
+                'user_id' => $admin->id,
+                'token' => $token,
+                'role_id' => $admin->role,
+                'last_login' => now(),
+                'status' => 1,
+            ]);
+
+            return helpers::response(1, 'Login Berhasil', [
+                'loginLogs' => $loginLogs,
+            ]);
+        }
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\Auth\LoginController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +17,17 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::group(['namespace' => 'api', 'prefix' => 'v1'], function () {
+    Route::get('testing', function () {
+        return response()->json([
+            'status' => 'success',
+            'message' => 'API Working good',
+        ], 200);
+    });
+
+    Route::prefix('auth')->group(function () {
+        Route::post('login', [LoginController::class, 'login']);
+    });
 });
